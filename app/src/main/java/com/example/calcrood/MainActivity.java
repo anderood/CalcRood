ba26a988a6b7e36758b836e898456c6f9169c741
anderood <anderood@hotmail.com>
2023-10-06 17:42:51 -0300

    refatoramento dos metodos de operação

--- app/src/main/java/com/example/calcrood/MainActivity.java
+++ app/src/main/java/com/example/calcrood/MainActivity.java
@@ -5,9 +5,17 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.DialogInterface;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+
+import java.util.HashMap;
+import java.util.Map;
+
+enum Operator {
+    ADDITION, SUBTRACTION, MULTIPLICATION, DIVISION
+}
 
 public class MainActivity extends AppCompatActivity {
 
@@ -18,41 +26,70 @@ public class MainActivity extends AppCompatActivity {
     private String numbersA;
     private String operator;
 
-
-    private Double calcResult(String numbersA, String numbersB, String operator){
-
-        if (operator.equals("+")) {
-            return Double.parseDouble(numbersA) + Double.parseDouble(numbersB);
-        } else if (operator.equals("-")) {
-            return Double.parseDouble(numbersA) - Double.parseDouble(numbersB);
-        } else if (operator.equals("᙮")) {
-            return Double.parseDouble(numbersA) * Double.parseDouble(numbersB);
+    private Double calcResult(Double numbersA, Double numbersB, Operator operator) {
+        switch (operator) {
+            case ADDITION:
+                return numbersA + numbersB;
+            case SUBTRACTION:
+                return numbersA - numbersB;
+            case MULTIPLICATION:
+                return numbersA * numbersB;
+            case DIVISION:
+                if (numbersB != 0) {
+                    return numbersA / numbersB;
+                }
+            default:
+                return Double.NaN;
         }
-
-        return Double.parseDouble(numbersA) / Double.parseDouble(numbersB);
     }
-
-    private String getNumbersB(String operator){
-
+    private String getNumbersB(Operator operator) {
         String numbersB = null;
 
         String idxTextView = textView.getText().toString();
-        int idxMais = idxTextView.indexOf(operator);
+        String operatorStr = "";
 
-        if (idxMais != -1 && idxMais < idxTextView.length() - 1) {
-            String caracteresAposMais = idxTextView.substring(idxMais + 1);
-            numbersB = caracteresAposMais;
+        switch (operator) {
+            case ADDITION:
+                operatorStr = "+";
+                break;
+            case SUBTRACTION:
+                operatorStr = "-";
+                break;
+            case MULTIPLICATION:
+                operatorStr = "᙮";
+                break;
+            case DIVISION:
+                operatorStr = "÷";
+                break;
+        }
+
+        int idxOperator = idxTextView.indexOf(operatorStr);
+
+        if (idxOperator != -1 && idxOperator < idxTextView.length() - 1) {
+            numbersB = idxTextView.substring(idxOperator + 1);
         }
 
         return numbersB;
-
     }
 
+
     private void viewNumber(String number){
-        if (textView.getText() != number) {
+        if (number.equals(textView.getText())) {
             textView.append(number);
         } else {
             textView.setText("0");
+        }
+    }
+    private void handleOperatorClick(Operator operator, String simbolo ) {
+        numbersA = String.valueOf(textView.getText());
+        this.operator = String.valueOf(operator);
+        textView.append(simbolo);
+    }
+    private void handleNumeroCick(String numero){
+        if("0".equals(textView.getText())){
+            textView.setText(numero);
+        }else {
+            textView.append(numero);
         }
     }
     @Override
@@ -86,146 +123,62 @@ public class MainActivity extends AppCompatActivity {
 
         btnInfo = findViewById(R.id.btnInfo);
 
+        View.OnClickListener numeroClickListener = new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                handleNumeroCick(((Button) view).getText().toString());
+            }
+        };
+
+        btn0.setOnClickListener(numeroClickListener);
+        btn1.setOnClickListener(numeroClickListener);
+        btn2.setOnClickListener(numeroClickListener);
+        btn3.setOnClickListener(numeroClickListener);
+        btn4.setOnClickListener(numeroClickListener);
+        btn5.setOnClickListener(numeroClickListener);
+        btn6.setOnClickListener(numeroClickListener);
+        btn7.setOnClickListener(numeroClickListener);
+        btn8.setOnClickListener(numeroClickListener);
+        btn9.setOnClickListener(numeroClickListener);
+
+        Map<Button, Operator> operatorMap = new HashMap<>();
+        operatorMap.put(btnPlus, Operator.ADDITION);
+        operatorMap.put(btnSub, Operator.SUBTRACTION);
+        operatorMap.put(btnMulti, Operator.MULTIPLICATION);
+        operatorMap.put(btnDivi, Operator.DIVISION);
+
+        View.OnClickListener operatorClickListener = new View.OnClickListener(){
+            @Override
+            public  void onClick(View view){
+                Operator operator1 = operatorMap.get(view);
+                String simbolo = ((Button) view).getText().toString();
+                handleOperatorClick(operator1, simbolo);
+            }
+        };
+
+        btnPlus.setOnClickListener(operatorClickListener);
+        btnSub.setOnClickListener(operatorClickListener);
+        btnMulti.setOnClickListener(operatorClickListener);
+        btnDivi.setOnClickListener(operatorClickListener);
+
         btnClean.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 textView.setText("");
                 textView2.setText("");
                 numbersA = "";
-                getNumbersB(operator);
             }
         });
-
-        btn0.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                viewNumber("0");
-            }
-        });
-
-        btn1.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-               viewNumber("1");
-            }
-        });
-
-        btn2.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                viewNumber("2");
-            }
-        });
-
-        btn3.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                viewNumber("3");
-            }
-        });
-
-        btn4.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                viewNumber("4");
-            }
-        });
-
-        btn5.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                viewNumber("5");
-            }
-        });
-
-        btn6.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                viewNumber("6");
-            }
-        });
-
-        btn7.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                viewNumber("7");
-            }
-        });
-
-        btn8.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                viewNumber("8");
-            }
-        });
-
-        btn9.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                viewNumber("9");
-            }
-        });
-
         btnDot.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if(textView.getText() == "0"){
+                if("0".equals(textView.getText())){
                     textView.setText(".");
                 }else {
                     textView.append(".");
                 }
             }
         });
-
-        btnPlus.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                numbersA = String.valueOf(textView.getText());
-                operator = "+";
-                textView.append("+");
-            }
-        });
-
-        btnSub.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                numbersA = String.valueOf(textView.getText());
-                operator = "-";
-                textView.append("-");
-            }
-        });
-
-        btnDivi.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                numbersA = String.valueOf(textView.getText());
-                operator = "÷";
-                textView.append("÷");
-            }
-        });
-
-        btnMulti.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                numbersA = String.valueOf(textView.getText());
-                operator = "᙮";
-                textView.append("᙮");
-            }
-        });
-
-        btnIgual.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-
-                textView2.setText(textView.getText().toString());
-
-                String number = getNumbersB(operator);
-
-                textView.setText(String.valueOf(calcResult(numbersA, number, operator)));
-
-            }
-        });
-
         btnBack.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -236,7 +189,6 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
-
         btnInfo.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -254,6 +206,26 @@ public class MainActivity extends AppCompatActivity {
                 dialog.show();
             }
         });
+        btnIgual.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String numeroB = getNumbersB(Operator.valueOf(operator));
+
+                if (numeroB != null) {
+                    textView2.setText(textView.getText().toString());
+
+                    Double numA = Double.parseDouble(numbersA);
+                    Double numB = Double.parseDouble(numeroB);
+
+                    Double resultado = calcResult(numA, numB, Operator.valueOf(operator));
+                    textView.setText(String.valueOf(resultado));
+                }
+
+                numbersA = "";
+                operator = "";
+            }
+        });
+
 
     }
 }
